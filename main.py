0a73119738667267fbbcb3318396f7c92d879a23
xElon-Mask <xelonmask@protonmail.com>
2022-09-21 16:35:57 +0200

    construction of the 1st question

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+1 - Ecrire la question :
+2 - Proposez 4 réponses : a, b, c, d avec toujours une seule réponse valide
+3 - Demander à l'utilisateur de donner sa réponse.
+4 - En fonction de sa réponse, afficher Bonne réponse / Mauvaise réponse
+5 - Ensuite, on passe à la 2eme question : Quelle est la capitale de l'Italie ?
+6 - et une troisième, et une quatrième
+"""
+
+print("Question : Quelle est la capitale de la France ?")
+print("a - Marseille")
+print("b - Nice")
+print("c - Paris")
+print("d - Nantes")
+rep_user = input("Votre réponse :")
