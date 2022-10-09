ea37e8f3e714952b36ad1511a060ba5fd3ef5409
xElon-Mask <xelonmask@protonmail.com>
2022-10-09 18:37:14 +0200

    fix Question class

--- questionnaire_v3_poo.py
+++ questionnaire_v3_poo.py
@@ -25,23 +25,41 @@ class Question:
         self.bonne_reponse = bonne_reponse
 
     def poser(self):
-        rep_user = input(self.titre)
-        if rep_user == self.bonne_reponse:
-            return True
-        return False
+        print("QUESTION")
+        print(("  ") + self.titre)
+        for i in range(len(self.choix)):
+            print("  ", i+1, "-", self.choix[i])
 
-def demander_reponse_numerique_utlisateur(min, max):
-    reponse_str = input("Votre réponse (entre " + str(min) + " et " + str(max) + ") :")
-    try:
-        reponse_int = int(reponse_str)
-        if min <= reponse_int <= max:
-            return reponse_int
+        print()
+        resultat_response_correcte = False
+        reponse_int = Question.demander_reponse_numerique_utlisateur(1, len(self.choix))
+        if self.choix[reponse_int-1].lower() == self.bonne_reponse.lower():
+            print("Bonne réponse")
+            resultat_response_correcte = True
+        else:
+            print("Mauvaise réponse")
+            
+        print()
+        return resultat_response_correcte
 
-        print("ERREUR : Vous devez rentrer un nombre entre", min, "et", max)
-    except:
-        print("ERREUR : Veuillez rentrer uniquement des chiffres")
-    return demander_reponse_numerique_utlisateur(min, max)
-    
+
+        # rep_user = input(self.titre)
+        # if rep_user == self.bonne_reponse:
+        #     return True
+        # return False
+
+    def demander_reponse_numerique_utlisateur(min, max):
+        reponse_str = input("Votre réponse (entre " + str(min) + " et " + str(max) + ") :")
+        try:
+            reponse_int = int(reponse_str)
+            if min <= reponse_int <= max:
+                return reponse_int
+
+            print("ERREUR : Vous devez rentrer un nombre entre", min, "et", max)
+        except:
+            print("ERREUR : Veuillez rentrer uniquement des chiffres")
+        return demander_reponse_numerique_utlisateur(min, max)
+        
 
 '''
 titre = question[0]
@@ -54,21 +72,7 @@ def poser_question(question):
     bonne_reponse = question[2]
     print("QUESTION")
     print("  " + question[0])
-    for i in range(len(choix)):
-        print("  ", i+1, "-", choix[i])
-
-    print()
-    resultat_response_correcte = False
-    reponse_int = demander_reponse_numerique_utlisateur(1, len(choix))
-    if choix[reponse_int-1].lower() == bonne_reponse.lower():
-        print("Bonne réponse")
-        resultat_response_correcte = True
-    else:
-        print("Mauvaise réponse")
-        
-    print()
-    return resultat_response_correcte
-
+    
 
 '''
     questionnaire[]
